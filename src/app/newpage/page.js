968fb63bc0e7ab3cc27496ab09993b2773b1653b
samrat-rock <samratpandey715@gmail.com>
2025-01-31 17:04:21 +0545

    removed tag

--- src/app/newpage/page.js
+++ src/app/newpage/page.js
@@ -20,9 +20,9 @@ const data = [
   ];
 
 
-  function Page() {
+ function Page() {
     return (
-      <div className="container mx-auto mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-[1200px]">
+      <div className="container mx-auto mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-[1400px] font-poppins">
         {data.concat(data).map((item, index) => (
           <div key={index} className="border-2 rounded-lg shadow-lg overflow-hidden flex flex-col">
             <div className="relative pt-[200px]">
@@ -33,19 +33,13 @@ const data = [
               />
             </div>
   
-            <div className="pt-4 pb-4 px-4 text-center font-serif flex flex-col flex-1">
+            <div className="pt-4 pb-4 px-4 text-center flex flex-col flex-1">
               <h3 className="font-bold text-lg mb-2">{item.title}</h3>
     
               <p className="text-left text-sm mb-4">
                 {item.description}
               </p>
-              <div className="flex flex-wrap gap-2 mt-auto">
-                {item.tags.map((tag, i) => (
-                  <span key={i} className="bg-gray-200 rounded-full px-3 py-1 text-sm">
-                    #{tag}
-                  </span>
-                ))}
-              </div>
+              
             </div>
           </div>
         ))}
