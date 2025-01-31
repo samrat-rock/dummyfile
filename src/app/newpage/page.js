874312e755b153c16594136386ca5efc3902d017
samrat-rock <samratpandey715@gmail.com>
2025-01-31 14:12:50 +0545

    demo

---
+++ src/app/newpage/page.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="container mx-auto border-2 border-black rounded-lg shadow-lg w-[400px] mt-5 p-3">
+      <img
+        className="h-auto max-w-full rounded-lg"
+        src="/docs/images/examples/image-1@2x.jpg"
+        alt="Example"
+      />
+
+      <div className="pt-[50px] pb-[30px] text-center font-serif">
+        <h3 className="font-bold">
+            The  Coldest Sunset
+        </h3>
+        <p className="text-left pl-[40px]">Lorem ipsum dolor sit amet, consectetur <br/>
+        adisping eilt. Voluptatiubs aquia, nulla!, <br/>
+        Marios et perferendis eaqua <br/>
+        exercitatioem praesentium nihil
+        </p>
+        <div className="flex gap-3 pl-[70px] py-3 " >
+            <p className="bg rounded-lg p-1">#photography</p>
+            <p className="bg rounded-lg p-1">#travel</p>
+            <p className="bg rounded-lg p-1">#winter</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function JobCategories() {
   const jobCategories = [
     {
@@ -9,7 +11,6 @@ export default function JobCategories() {
         "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
       ],
     },
-
     {
       id: 2,
       image:
@@ -19,7 +20,6 @@ export default function JobCategories() {
         "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
-
     {
       id: 3,
       image:
@@ -33,80 +33,92 @@ export default function JobCategories() {
     {
       id: 4,
       image:
-        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
-      name: "Security",
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
       roles: [
-        "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
 
     {
       id: 5,
       image:
-        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
-      name: "Security",
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
       roles: [
-        "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
 
     {
       id: 6,
       image:
-        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
-      name: "Security",
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
       roles: [
-        "Security Supervisor, Security Foremen",
-        " Personal Bodyguard, Security Guard",
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
 
     {
       id: 7,
       image:
-        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
-      name: "Security",
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
       roles: [
-        "Security Supervisor, Security Foremen",
-        " Personal Bodyguard, Security Guard",
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
 
     {
       id: 8,
       image:
-        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
-      name: "Security",
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
       roles: [
-        "Security Supervisor, Security Foremen",
-        " Personal Bodyguard, Security Guard",
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
+
+    
   ];
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6">Available Job Categories</h1>
-      <div className="grid  lg:grid-cols-2 gap-4">
-        {jobCategories.map((category) => (
-          <div
-            key={category.id}
-            className="bg-white p- rounded-lg shadow-md flex items-center gap-4"
-          >
-            <img src={category.image} alt={category.name} className="w-32 h-32 object-cover rounded-lg img" />
-            <div>
-              <h2 className="text-xl font-semibold mb-3">{category.name}</h2>
-              <ul>
-                {category.roles.map((role, index) => (
-                  <li key={index} className="text-gray-700 mb-1">
-                    {role}
-                  </li>
-                ))}
-              </ul>
+    <>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-6 text-center">
+          Available Job Categories
+        </h1>
+        <div className="grid lg:grid-cols-2 gap-4">
+          {jobCategories.map((category) => (
+            <div
+              key={category.id}
+              className="bg-white p-6 rounded-lg shadow-md flex items-center gap-4 border-4 border-gray-600"
+            >
+              <img
+                src={category.image}
+                alt={category.name}
+                className="object-cover rounded-lg img"
+              />
+              <div>
+                <h2 className="text-xl font-semibold mb-3">{category.name}</h2>
+                <ul>
+                  {category.roles.map((role, index) => (
+                    <li key={index} className="text-gray-700 mb-1">
+                      {role}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+
+      
+      <div className="flex justify-center mt-6">
+       <Link href='/newpage'><button className="btn ">NEXT PAGE</button></Link> 
+      </div>
+    </>
   );
 }
