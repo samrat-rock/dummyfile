ddd4f721246a7c2bb00769fb8091363fbfbf3c3b
samrat-rock <samratpandey715@gmail.com>
2025-01-31 12:38:15 +0545

    arrya and object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,112 @@
-import Image from "next/image";
+export default function JobCategories() {
+  const jobCategories = [
+    {
+      id: 1,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
+      ],
+    },
 
-export default function Home() {
+    {
+      id: 2,
+      image:
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
+      roles: [
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
+      ],
+    },
+
+    {
+      id: 3,
+      image:
+        "https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Construction",
+      roles: [
+        "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
+      ],
+    },
+
+    {
+      id: 4,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
+      ],
+    },
+
+    {
+      id: 5,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen, Personal Bodyguard, Security Guard",
+      ],
+    },
+
+    {
+      id: 6,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen",
+        " Personal Bodyguard, Security Guard",
+      ],
+    },
+
+    {
+      id: 7,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen",
+        " Personal Bodyguard, Security Guard",
+      ],
+    },
+
+    {
+      id: 8,
+      image:
+        "https://img.freepik.com/free-photo/medium-shot-security-officer-posing-studio_23-2150275721.jpg",
+      name: "Security",
+      roles: [
+        "Security Supervisor, Security Foremen",
+        " Personal Bodyguard, Security Guard",
+      ],
+    },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6">Available Job Categories</h1>
+      <div className="grid  lg:grid-cols-2 gap-4">
+        {jobCategories.map((category) => (
+          <div
+            key={category.id}
+            className="bg-white p- rounded-lg shadow-md flex items-center gap-4"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <img src={category.image} alt={category.name} className="w-32 h-32 object-cover rounded-lg img" />
+            <div>
+              <h2 className="text-xl font-semibold mb-3">{category.name}</h2>
+              <ul>
+                {category.roles.map((role, index) => (
+                  <li key={index} className="text-gray-700 mb-1">
+                    {role}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
