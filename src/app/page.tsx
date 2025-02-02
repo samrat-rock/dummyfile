41b5757f1f4ca753d3af1d8f025cf178b752a682
samrat-rock <samratpandey715@gmail.com>
2025-02-02 10:38:15 +0545

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,6 @@ export default function JobCategories() {
         "Engineer(civil, Electric, Mechanical)/Surveyor(land and Quantity) Overseer, Supervisor, Drafts man foremen, Accountant, Time Keeper, Steel Fixer,plumber, Weilder, Rigger, Scaffolder",
       ],
     },
-
     {
       id: 7,
       image:
@@ -85,40 +84,37 @@ export default function JobCategories() {
 
   return (
     <>
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-6 text-center">
-          Available Job Categories
-        </h1>
-        <div className="grid lg:grid-cols-2 gap-4">
-          {jobCategories.map((category) => (
-            <div
-              key={category.id}
-              className="bg-white p-1 rounded-lg shadow-md flex items-center gap-4 border-4 border-gray-600"
-            >
+    <div className="container mx-auto mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-w-[1200px] font-poppins">
+        {jobCategories.concat(jobCategories).map((item, index) => (
+          <div
+            key={index}
+            className="border-2 rounded-lg shadow-md overflow-hidden flex flex-col w-[250px] h-[350px] mx-auto"
+          >
+            <div className="relative pt-[150px]">
               <img
-                src={category.image}
-                alt={category.name}
-                className="object-cover rounded-lg img"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                src={item.image}
+                alt={item.name}
               />
-              <div>
-                <h2 className="text-xl font-semibold mb-3">{category.name}</h2>
-                <ul>
-                  {category.roles.map((role, index) => (
-                    <li key={index} className="text-gray-700 mb-1">
-                      {role}
-                    </li>
-                  ))}
-                </ul>
-              </div>
             </div>
-          ))}
-        </div>
+
+            <div className="pt-3 pb-3 px-3 text-center flex flex-col flex-1">
+              <h3 className="font-serif text-md mb-2 text-left">{item.roles}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-4">
+        <Link href="/newpage">
+          <button className="btn px-4 py-2 bg-blue-500 text-white rounded-lg">
+            NEXT PAGE
+          </button>
+        </Link>
       </div>
 
       
-      <div className="flex justify-center mt-6">
-       <Link href='/newpage'><button className="btn ">NEXT PAGE</button></Link> 
-      </div>
+     
     </>
   );
 }
